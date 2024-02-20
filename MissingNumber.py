114efb5f30624ce69fa7983d57eda858b56d9195
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-02-20 22:41:24 +0530

    Add files via upload

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,29 +1,39 @@
-def miss(a,b):
-    count={}
-    if(len(a)==0):
-        return 0
-    for i in a:
-        if i in count:
-            count[i]+=1
-        else:
-            count[i]=1    
-    for j in b:
-        if j in count:
-            count[j]-=1
-        else:
-            count[j]=1
-    for k in count:
-        if count[k]==1:
-            return k
-        
-        
-        
-d=miss([1,2,3,4,5,6,7],[3,7,2,1,4,6])
-print(d)    
+"""Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
+ 
 
+Example 1:
 
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
 
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
 
-    
-                            
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+ 
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        a=b=0
+        for  i in range(len(nums)+1):
+            a^=i
+        for i in nums:
+            b^=i
+        return a^b
