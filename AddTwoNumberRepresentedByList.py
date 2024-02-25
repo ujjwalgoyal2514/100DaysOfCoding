31b0ed1f8c93108097bc58963b9643d61ddfb51a
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-02-25 12:02:14 +0530

    Add files via upload

---
+++ AddTwoNumberRepresentedByList.py
@@ -0,0 +1,68 @@
+"""Given two decimal numbers represented by two linked lists of size N and M respectively. The task is to return a linked list that represents the sum of these two numbers.
+
+For example, the number 190 will be represented by the linked list, 1->9->0->null, similarly 25 by 2->5->null. Sum of these two numbers is 190 + 25 = 215, which will be represented by 2->1->5->null. You are required to return the head of the linked list 2->1->5->null.
+
+Example 1:
+
+Input:
+N = 2
+valueN[] = {4,5}
+M = 3
+valueM[] = {3,4,5}
+Output: 3 9 0  
+Explanation: For the given two linked
+list (4 5) and (3 4 5), after adding
+the two linked list resultant linked
+list will be (3 9 0).
+Example 2:
+
+Input:
+N = 2
+valueN[] = {6,3}
+M = 1
+valueM[] = {7}
+Output: 7 0
+Explanation: For the given two linked
+list (6 3) and (7), after adding the
+two linked list resultant linked list
+will be (7 0).
+Your Task:
+The task is to complete the function addTwoLists() which has node reference of both the linked lists and returns the head of the sum list.   
+
+Expected Time Complexity: O(N+M)
+Expected Auxiliary Space: O(Max(N,M)) for the resultant list.
+
+Constraints:
+1 <= N, M <= 5000
+"""
+''' Node for linked list:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    #Function to add two numbers represented by linked list.
+    def addTwoLists(self, first, second):
+        # code here
+        # return head of sum list
+        str2=""
+        itr=first
+        s=0
+        itr1=second
+        str1=""
+        while itr:
+            str1+=str(itr.data)
+            itr=itr.next
+        while itr1:
+            str2+=str(itr1.data)
+            itr1=itr1.next
+        s=int(str1)+int(str2)
+        new=Node(-1)
+        ans=new
+        for i in str(s):
+            ans.next=Node(int(i))
+            ans=ans.next
+        return new.next
