25c42bba6f0fd0d5ef44565c2d2f111de47df2bb
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-02-27 10:29:19 +0530

    Add files via upload

---
+++ reverseByGroupinLL.py
@@ -0,0 +1,54 @@
+"""Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Example 1:
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+Example 2:
+
+Input:
+LinkedList: 1->2->3->4->5
+K = 3
+Output: 3 2 1 5 4 
+Explanation: 
+The first 3 elements are 1,2,3 are reversed 
+first and then elements 4,5 are reversed.Hence, 
+the resultant linked list is 3->2->1->5->4.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+"""
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def reverse(self,head, k):
+        # Code here
+        rev=None
+        itr=head
+        c=0
+        while itr and c<k:
+            next=itr.next
+            itr.next=rev
+            rev=itr
+            itr=next
+            c+=1
+        if next :
+            head.next=self.reverse(next,k)
+        return rev
