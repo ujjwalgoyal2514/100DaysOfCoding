3ef58e46e38768921b086942364a5c3f92f67248
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-03-03 16:05:11 +0530

    Add files via upload

---
+++ largestNumberFormedfromArray.py
@@ -0,0 +1,31 @@
+"""Given an array of strings arr[] of length n representing non-negative integers, arrange them in a manner, such that, after concatanating them in order, it results in the largest possible number. Since the result may be very large, return it as a string.
+
+Example 1:
+
+Input: 
+n = 5
+arr[] =  {"3", "30", "34", "5", "9"}
+Output: "9534330"
+Explanation: 
+Given numbers are  {"3", "30", "34", "5", "9"}, 
+the arrangement "9534330" gives the largest value.
+Example 2:
+
+Input: 
+n = 4
+arr[] =  {"54", "546", "548", "60"}
+Output: "6054854654"
+Explanation: 
+Given numbers are {"54", "546", "548", "60"}, the 
+arrangement "6054854654" gives the largest value.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function printLargest() which takes the array of strings arr[] as a parameter and returns a string denoting the answer.
+
+Expected Time Complexity: O(n*log(n) ).
+Expected Auxiliary Space: O(n).
+"""
+from functools import cmp_to_key
+class Solution:
+	def printLargest(self, n, arr):
+	    # code here
+	    return str(int("".join(sorted(arr, key= cmp_to_key(lambda a,b : -1 if (a+b) > (b+a) else 1 )))))
