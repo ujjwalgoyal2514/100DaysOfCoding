9fa817385636779004ebdf8a4eed282ad2549055
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-03-06 07:00:10 +0530

    Add files via upload

---
+++ searchPatternRobinKarpMethod.py
@@ -0,0 +1,29 @@
+"""Given two strings, one is a text string and other is a pattern string. The task is to print the indexes of all the occurences of pattern string in the text string. For printing, Starting Index of a string should be taken as 1. The strings will only contain lowercase English alphabets ('a' to 'z').
+
+Example 1:
+
+Input: 
+text = "birthdayboy"
+pattern = "birth"
+Output: 
+[1]
+Explanation: 
+The string "birth" occurs at index 1 in text.
+Example 2:
+
+Input:
+text = "geeksforgeeks"
+pattern = "geek"
+Output: 
+[1, 9]
+Explanation: 
+The string "geek" occurs twice in text, one starts are index 1 and the other at index 9.
+"""
+class Solution:
+    def search(self, pattern, text):
+        # code here
+        l=[]
+        for i in range(len(text)-len(pattern)+1):
+            if(text[i:i+len(pattern)])==pattern:
+                l.append(i+1)
+        return l
