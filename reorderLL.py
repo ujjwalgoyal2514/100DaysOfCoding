220a50c26c72be40459667a2d743ba7d0a84d34a
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-03-23 10:21:59 +0530

    Add files via upload

---
+++ reorderLL.py
@@ -0,0 +1,67 @@
+"""You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverse(self, head):
+        if not head:
+            return None
+        prev = None
+        curr = head
+        nextNode = None
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev
+
+    def merge(self, list1, list2):
+        while list2:
+            nextNode = list1.next
+            list1.next = list2
+            list1 = list2
+            list2 = nextNode
+
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head
+        prev = head
+        while fast and fast.next:
+            prev = slow
+            fast = fast.next.next
+            slow = slow.next
+        prev.next = None
+        list1 = head
+        list2 = self.reverse(slow)
+        self.merge(list1, list2)
