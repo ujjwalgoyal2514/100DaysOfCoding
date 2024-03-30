ccb574b8c16e03936d276fb959a3a2483d8da401
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-03-30 19:20:53 +0530

    Add files via upload

---
+++ minimumElementinBST.py
@@ -0,0 +1,40 @@
+"""
+Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST.
+
+Example 1:
+
+Input:
+           5
+         /    \
+        4      6
+       /        \
+      3          7
+     /
+    1
+Output: 1
+Example 2:
+
+Input:
+             9
+              \
+               10
+                \
+                 11
+Output: 9
+Your Task:
+The task is to complete the function minValue() which takes root as the argument and returns the minimum element of BST. If the tree is empty, there is no minimum element, so return -1 in that case.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(1).
+
+Constraints:
+0 <= n <= 104
+
+
+"""
+
+class Solution:
+    #Function to find the minimum element in the given BST.
+    def minValue(self, root):
+        ##Your code here
+        return self.minValue(root.left) if root.left !=None else root.data
