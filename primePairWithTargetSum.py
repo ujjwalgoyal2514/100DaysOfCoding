f1bbdd28e4aaa66de5d3d056d5aaae9f7a343252
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2024-06-16 09:16:46 +0530

    Add files via upload

---
+++ primePairWithTargetSum.py
@@ -0,0 +1,44 @@
+"""Given a number n, find out if n can be expressed as a+b, where both a and b are prime numbers. If such a pair exists, return the values of a and b, otherwise return [-1,-1] as an array of size 2.
+Note: If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, and a < c then  [a, b] is considered as our answer.
+
+Examples
+
+Input: n = 10
+Output: 3 7
+Explanation: There are two possiblities 3, 7 & 5, 5 are both prime & their sum is 10, but we'll pick 3, 7 as 3 < 5.
+Input: n = 3
+Output: -1 -1
+Explanation: There are no solutions to the number 3.
+Expected Time Complexity: O(n*loglog(n))
+Expected Auxiliary Space: O(n)
+
+Constraints:
+2 <= n <= 106
+
+"""
+from typing import List
+
+class Solution:
+    def getPrimes(self, n: int) -> List[int]:
+        def is_prime(x):
+            if x <= 1:
+                return False
+            if x <= 3:
+                return True
+            if x % 2 == 0 or x % 3 == 0:
+                return False
+            i = 5
+            while i * i <= x:
+                if x % i == 0 or x % (i + 2) == 0:
+                    return False
+                i += 6
+            return True
+
+        a, b = -1, -1
+        for i in range(1, (n // 2) + 1):
+            if is_prime(i) and is_prime(n - i):
+                a = i
+                b = n - i
+                break
+
+        return [a, b]
